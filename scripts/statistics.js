7326d450bc12ba6ca8a858420dbb61285235b06e
Fendy H <fendy@numoney.store>
2021-07-09 16:58:07 +0700

    updated dividend payout

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -1,6 +1,6 @@
 $.when($.ready).then(function () {
     const raisedFunds = 69195000000;
-    const dividendPayout = 1716720000;
+    const dividendPayout = 1773420000;
 
     fetch('https://mobile.landx.id/mobile/landing_data')
         .then(data => data.json())
