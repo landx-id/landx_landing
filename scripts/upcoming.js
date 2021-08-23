2483d14f8891f93bc9c1b3cb935d1bfd066caf8f
almibiakmal <almibi@landx.id>
2021-08-23 15:05:38 +0700

    Add card in index, add upcoming JSON and update LP kapal cumi

--- scripts/upcoming.js
+++ scripts/upcoming.js
@@ -1,3 +1,7 @@
+function detail(url){
+  location.href = url;
+}
+
 $.getJSON("lottie/upcoming.json", function(dataUpcoming) {
   
   let temp = [];
@@ -18,23 +22,27 @@ $.getJSON("lottie/upcoming.json", function(dataUpcoming) {
     return time_a > time_b ? -1 : 1
   });
 
-  for(let i = temp.length - 1; i >= 0; i--){
-    let expiredAt = new Date(temp[i].listing_at).getTime(); //convert date and time to unix time
-    let distance = expiredAt - now;
-    let cardUpcoming = `
-    <div class="col-12 col-md-auto col-xl-3" onClick="detail('${temp[i].link}')">
-      <div class="card custom">
-        <img class="thumnail" src="${temp[i].images}">
-        <img class="label-soon" src="assets/soon-listing.png">
-        <h5 class="title-thumnail">${temp[i].title}</h5>
-        <div class="canv">
-          <span class="count-down-thumnail text-count-down-thumnail">${Math.floor(distance / (1000 * 60 * 60 * 24))} Hari : ${Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Jam</span>
+  setTimeout(function(){
+    for(let i = temp.length - 1; i >= 0; i--){
+      let expiredAt = new Date(temp[i].listing_at).getTime(); //convert date and time to unix time
+      let distance = expiredAt - now;
+      let cardUpcoming = `
+      <div class="col-12 col-md-auto col-xl-3" onClick="detail('${temp[i].link}')">
+        <div class="card custom">
+          <img class="thumnail" src="${temp[i].images}">
+          <img class="label-soon" src="img/soon-listing.png">
+          <h5 class="title-thumnail">${temp[i].title}</h5>
+          <div class="canv">
+            <span class="count-down-thumnail text-count-down-thumnail">${Math.floor(distance / (1000 * 60 * 60 * 24))} Hari : ${Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Jam</span>
+          </div>
+          <p class="detial-thumnail">PELAJARI LEBIH LANJUT</p>
         </div>
-        <p class="detial-thumnail">PELAJARI LEBIH LANJUT</p>
-      </div>
-    </div>`;
+      </div>`;
 
-    document.getElementById("card-base").children[0].children[0].insertAdjacentHTML('afterbegin', cardUpcoming);
-  }
-
+      document.getElementById("card-base").children[0].children[0].insertAdjacentHTML('afterbegin', cardUpcoming);   
+    }
+  }, 5000); 
 });
+
+
+
