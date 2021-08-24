d2979d82ebf73d6411e170a71679c26aa78261dd
almibiakmal <almibi@landx.id>
2021-08-25 02:45:17 +0700

    Add and update file LP kapalcumi

--- lp/kapalcumi/scripts/detailUpcoming.js
+++ lp/kapalcumi/scripts/detailUpcoming.js
@@ -1,5 +1,5 @@
 const generateDetailUpcoming = (data) => {
-    const expiredAt = new Date(data.expired_at).getTime(); //convert date and time to unix time
+    const expiredAt = new Date(data.listing_at).getTime(); //convert date and time to unix time
     const now = Date.now();
     const distance = expiredAt - now;
 
@@ -13,20 +13,20 @@ const generateDetailUpcoming = (data) => {
                 ${data.description}
             </div>
             <div class="row info">
-                <div>
+                <div class="subInfo">
                     <h6 class="caption-project">Harga Per Lot</h6>
                     <h4 class="caption-val-project" id="price-lot-upcoming">Rp ${numeral(data.initialTokenPrice).format("0,0")}</h4>
                 </div>
-                <div>
+                <div class="subInfo">
                     <h6 class="caption-project periode">Periode Dividen</h6>
                     <h4 class="caption-val-project val-periode" id="periode-upcoming">${data.dividendSchedule}</h4>
                 </div>
-                <div>
+                <div class="subInfo">
                     <h6 class="caption-project deviden">Estimasi Dividen</h6>
                     <h4 class="caption-val-project val-deviden" id="deviden-upcoming">${parseFloat(data.annualRentYield) * 100}-${parseFloat(data.annualRentYieldUpper) * 100}% Per Periode</h4>
                 </div>
             </div>
-            <div class="row flex-sm-row d-flex-inline" style="background-color: #fff; width:fit-content; border-radius: 12px; margin: 30px 0px 0px 0px;">
+            <div class="row flex-sm-row d-flex-inline kotakPutih-bawah storeContainer">
                 <h5 class="countDown">${Math.floor(distance / (1000 * 60 * 60 * 24))} Hari : ${Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Jam</h5>
                 <a href="https://play.google.com/store/apps/details?id=store.numoney.landxapp"> <img src="../../img/Play Store.svg" alt="Play Store" > </a>
                 <a href="https://apps.apple.com/id/app/landx/id1453823676"> <img src="../../img/App Store.svg" alt="App Store" > </a>
@@ -34,7 +34,7 @@ const generateDetailUpcoming = (data) => {
         </div>
     </div>`;
 }
-$.getJSON("data/upcoming.json", function(data) {
+$.getJSON("../../lottie/upcoming.json", function(data) {
     console.log(data); // this will show the info it in firebug console
     var upcoming_detail = document.getElementById("root");
     upcoming_detail.innerHTML = generateDetailUpcoming(data.upcoming[0]["kapal-cumi"]);
