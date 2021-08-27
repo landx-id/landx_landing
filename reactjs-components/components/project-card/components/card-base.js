28c260afba6b65aa7683d39a44c7442c58a1d9df
raflylesmana3003 <raflylesmana111@gmail.com>
2021-08-27 17:54:18 +0700

    fecth tested

--- reactjs-components/components/project-card/components/card-base.js
+++ reactjs-components/components/project-card/components/card-base.js
@@ -10,8 +10,18 @@ export const CardBase = (props) => {
     for (let i = 0; i < props.length; i++) {
         cards.push(
             <div className="col-12 col-md-auto col-xl-3" key={i} onClick={() => {
-               
-                window.open( props[i].link);
+                // check for project page redirect.
+                // we check if card project in homepage [landx.id] then we do normal redirect
+                // and if card project shown in [landx.id/project] we do change the url properties symbol then reload page 
+                // check if url doesnt have hash(#) path
+                if (window.location.pathname.indexOf("project") >= 1) {
+                    location.href = props[i].link;
+                    console.log
+                    location.reload();
+                } else {
+                    location.href = props[i].link; 
+                }
+                window.scrollTo( props[i].link);
             }}>
             <div className="card custom">
                     <CardCarousel
