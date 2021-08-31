8c7f7d6373efa39df8964c479defdbedae6c0a28
almibiakmal <almibi@landx.id>
2021-08-31 11:07:15 +0700

    Restructure project landing page for reusable

--- lp/assets/scripts/detailUpcoming.js
+++ lp/assets/scripts/detailUpcoming.js
@@ -4,10 +4,11 @@ const generateDetailUpcoming = (data) => {
     const distance = expiredAt - now;
     const day = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const images = screen.width < 768 ? data.images[2] : data.images[1];
 
     return `
     <div class="container">
-        <div class="container-upcoming">
+        <div class="container-upcoming" id="container-upcoming" style="background-image: url('${images}')">
             <div class="title-project-upcoming" id="title-upcoming">
                 ${data.title}
             </div>
@@ -25,19 +26,41 @@ const generateDetailUpcoming = (data) => {
                 </div>
                 <div class="subInfo">
                     <h6 class="caption-project deviden">Estimasi Dividen</h6>
-                    <h4 class="caption-val-project val-deviden" id="deviden-upcoming">${parseFloat(data.annualRentYield) * 100}-${parseFloat(data.annualRentYieldUpper) * 100}% Per Periode</h4>
+                    <h4 class="caption-val-project val-deviden" id="deviden-upcoming">${parseFloat(data.annualRentYield) * 100}-${parseFloat(data.annualRentYieldUpper) * 100}% Per Tahun</h4>
                 </div>
             </div>
             <div class="row flex-sm-row d-flex-inline whiteBox-under storeContainer">
                 <h5 class="countDown">${day > 0 ? day : 0} Hari : ${hours > 0 ? hours : 0} Jam</h5>
-                <a href="https://play.google.com/store/apps/details?id=store.numoney.landxapp"> <img src="../../img/Play Store.svg" alt="Play Store" > </a>
-                <a href="https://apps.apple.com/id/app/landx/id1453823676"> <img src="../../img/App Store.svg" alt="App Store" > </a>
+                <a href="https://play.google.com/store/apps/details?id=store.numoney.landxapp"> <img src="../img/Play Store.svg" alt="Play Store" > </a>
+                <a href="https://apps.apple.com/id/app/landx/id1453823676"> <img src="../img/App Store.svg" alt="App Store" > </a>
             </div>
         </div>
     </div>`;
 }
-$.getJSON("../../lottie/upcoming.json", function(data) {
-    console.log(data); // this will show the info it in firebug console
-    var upcoming_detail = document.getElementById("root");
-    upcoming_detail.innerHTML = generateDetailUpcoming(data.upcoming[0]["kapal-cumi"]);
+
+$(document).ready(function () {
+    var url = $(location).attr('href'),
+        parts = url.split("/"),
+        last_part = parts[parts.length - 2];
+
+    if (last_part == "#") {
+        last_part = parts[parts.length - 1];
+
+        $.getJSON("../lottie/upcoming.json", function(data) {
+            let isExistProject = false;
+            let temp = [];
+            data.upcoming.forEach((v, i) => {
+                if(last_part.toLowerCase() === Object.keys(v)[0]){
+                    isExistProject = true;
+                    temp.push({'index':i, 'name_project':last_part.toLowerCase()});
+                    return;
+                }
+            });
+
+            if(isExistProject){
+                var upcoming_detail = document.getElementById("root");
+                upcoming_detail.innerHTML = generateDetailUpcoming(data.upcoming[temp[0].index][temp[0].name_project]);
+            }
+        });
+    }
 });
