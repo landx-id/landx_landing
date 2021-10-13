8c3c96ef96c4c5eb4e3b437af12e085402832bc5
Almibi Akmal <almibi@landx.id>
2021-10-13 15:02:58 +0700

    update lable button CTA, height card and solved issue section current project

--- reactjs-components/components/project-card/components/card-base.js
+++ reactjs-components/components/project-card/components/card-base.js
@@ -3,50 +3,52 @@ import { CardCarousel } from './card-carousel.js';
 import { CardBody } from './card-body.js';
 import "../stylesheets/card.css";
 
+import data from '../../../../lottie/upcoming.json';
+
 export const CardBase = (props) => {
     let cards = [];
 
     /* Take an array of landXProperty's values */
-    $.getJSON("/lottie/upcoming.json", function(dataUpcoming) {
-        let temp = [];
-        const now = Date.now();
-        
-        dataUpcoming.upcoming.forEach(val => {
-          let key = Object.keys(val);
-          let listing_at = new Date(val[key].listing_at).getTime();
-          
-          if(listing_at > now){
+    let temp = [];
+    const now = Date.now();
+    
+    data.upcoming.forEach(val => {
+        let key = Object.keys(val);
+        let listing_at = new Date(val[key].listing_at).getTime();
+        if(listing_at > now){
             temp.push(val[key]);
-          }
-        });
-      
-        if(temp.length > 0){
-      
-            let limit = temp.length > 4 ? (temp.length - 4) : 0;
-            
-            for(let i = temp.length - 1; i >= limit; i--){
-              let expiredAt = new Date(temp[i].listing_at).getTime(); //convert date and time to unix time
-              let distance = expiredAt - now;
-                cards.push(
-                <div class="col-auto mx-auto mb-5" onClick={() => {
-                    location.href = temp[i].link; 
-
-                    }}>
-                    <div class="card custom">
-                        <img class="thumnail" src={"/lp/"+temp[i].images[0]}/>
-                        <img class="label-soon" src="/img/soon-listing.png"/>
-                        <h5 class="title-thumnail">{temp[i].title}</h5>
-                    <div class="canv">
-                        <span class="count-down-thumnail text-count-down-thumnail">{Math.floor(distance / (1000 * 60 * 60 * 24))} Hari : {Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Jam</span>
-                    </div>
-                    <p class="detial-thumnail">PELAJARI LEBIH LANJUT</p>
-                    </div>
-              </div>
-                );
-            }
         }
+    });
 
-    for (let i = 0; i < props.length; i++) {
+    if(temp.length > 0){
+        //Sorting upcoming project
+        temp = temp.sort((a, b) =>{
+            let time_a = new Date(a.listing_at).getTime();
+            let time_b = new Date(b.listing_at).getTime();
+            return time_a > time_b ? -1 : 1
+        });
+
+        let i = temp.length - 1;
+        let expiredAt = new Date(temp[i].listing_at).getTime(); //convert date and time to unix time
+        let distance = expiredAt - now;
+        cards.push(
+        <div class="col-auto mx-auto mb-5" onClick={() => {
+            location.href = temp[i].link; 
+            }}>
+            <div class="card custom">
+                <img class="thumnail" src={"/lp/"+temp[i].images[0]}/>
+                <img class="label-soon" src="/img/soon-listing.png"/>
+                <h5 class="title-thumnail">{temp[i].title}</h5>
+            <div class="canv">
+                <span class="count-down-thumnail text-count-down-thumnail">{Math.floor(distance / (1000 * 60 * 60 * 24))} Hari : {Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Jam</span>
+            </div>
+            <p class="detial-thumnail">PELAJARI LEBIH LANJUT</p>
+            </div>
+        </div>
+        );
+    }
+
+    for (let i = 0; i < (4 - temp.length); i++) {
         cards.push(
             <div className="col-auto mx-auto mb-5" key={i} onClick={() => {
                 // check for project page redirect.
@@ -72,9 +74,7 @@ export const CardBase = (props) => {
             </div>
         );
     }
-      });
-
-    
 
     return (cards);
+    
 }
