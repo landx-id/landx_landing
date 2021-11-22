36758fe05751666f0b4c5dae70eab6f8b114795e
Glend Maatita <glend.maatita@gmail.com>
2021-11-22 14:52:53 +0700

    Revert "Update"

--- reactjs-components/components/project-review-page/components/initialize-project.js
+++ reactjs-components/components/project-review-page/components/initialize-project.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import { CarouselGallery } from "./carousel-gallery";
 import { ProjectDescDetail } from "./project-review";
 
-export const initializeReviewProject = (project) => {
+export const initializeProject = (project) => {
     ReactDOM.render(
         <div>
-            <div className="container">
-                <div className="row mx-auto col-lg-6 col-md-12 col-sm-12">
-                    <div className="headlineTitle backToAllProject" id="backToAllProject">
-                        <a href="../project">
-                            <img id="backButton" src="/img/atom-icon-arrow-6.svg"/>
-                            <span className="text-primary button-back">Kembali ke Semua Proyek</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
             <div className="container mt-2">
                 <div className="row justify-content-center">
                     <CarouselGallery
@@ -26,8 +16,6 @@ export const initializeReviewProject = (project) => {
             <div className="container">
                 <ProjectDescDetail
                     description={project.description}
-                    day={project.day}
-                    hour={project.hour}
                 />
             </div>
         </div>
