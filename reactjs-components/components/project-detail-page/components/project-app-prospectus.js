823655804e46fa03f4871106ca96dbf0b026acec
Almibi Akmal <almibi@landx.id>
2021-12-02 15:30:21 +0700

    add video explanation on web and add prosepektus on web in pop up not redirect

--- reactjs-components/components/project-detail-page/components/project-app-prospectus.js
+++ reactjs-components/components/project-detail-page/components/project-app-prospectus.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+import { MobilePDFReader } from 'react-read-pdf'
 
 export const ProjectAppProspectus = (props) => {
+    const [showModal, setShowModal] = useState(false);
     return (
         <div className="row row-buttons">
             <div className="col-lg-6 col-md-12 col-sm-12" id="button-buy">
@@ -11,12 +14,42 @@ export const ProjectAppProspectus = (props) => {
                 </a>
             </div>
             <div className="col-lg-6 col-md-12 col-sm-12">
-                <a href={props.prospectusLink}>
-                    <button className="btn btn-outline-success btn-detail-bottom" style={{width: "100%"}}>
-                        DOWNLOAD PROSPEKTUS
-                    </button>
-                </a>
+                <button className="btn btn-outline-success btn-detail-bottom" onClick={()=>setShowModal(true)} style={{width: "100%"}}>
+                    SHOW PROSPEKTUS
+                </button>
             </div>
+            <Modal
+            isOpen={showModal}
+            onRequestClose={()=>setShowModal(false)}
+            style={{
+                overlay: {
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  zIndex: 1050,
+                  backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                },
+                content: {
+                  position: 'absolute',
+                  top: '10px',
+                  left: '10px',
+                  right: '10px',
+                  bottom: '10px',
+                  border: '1px solid #ccc',
+                  background: '#fff',
+                  overflow: 'none',
+                  WebkitOverflowScrolling: 'touch',
+                  borderRadius: '4px',
+                  outline: 'none'
+                }
+              }}
+            >
+                <MobilePDFReader url={props.prospectusLink} overflow={'none'} scale={'auto'} isShowFooter={false} isShowHeader={false}/>
+
+            </Modal>
+            
         </div>
     );
 }
