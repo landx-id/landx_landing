e841d489232147ea4c22bbd017a5105be6e804ff
ASUS\Fraza <adityafraja12@gmail.com>
2021-12-27 17:34:06 +0700

    fix tooltip

--- reactjs-components/components/project-detail-page/components/project-dividend-detail.js
+++ reactjs-components/components/project-detail-page/components/project-dividend-detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 export const ProjectDividendDetail = (props) => {
     return (
@@ -6,12 +7,14 @@ export const ProjectDividendDetail = (props) => {
             <div className="col">
                 <div className="row">
                     <div className="col-auto pl-0">
-                        <p className="detail-2" style={{display: "inline"}}>
-                            Periode Dividen <i className="iconify" data-icon="ant-design:info-circle-outlined"
-                                                data-toggle="tooltip"
-                                                style={{fontSize: "24px", color: "#2dbe60", marginBottom: "-5px"}}
-                                                title="Periode dividen aktual akan tetap mengacu pada laporan keuangan dan persetujuan Rapat Umum Pemegang Saham (RUPS).">
-                                            </i>
+                        <p className="detail-2" style={{ display: "inline" }}>
+                            Periode Dividen <a data-tip="Periode dividen aktual akan tetap mengacu pada laporan keuangan dan persetujuan Rapat Umum Pemegang Saham (RUPS)."
+                                data-event='click focus'>
+                                <i className="iconify"
+                                    data-icon="ant-design:info-circle-outlined"
+                                    style={{ fontSize: "24px", color: "#2dbe60", marginBottom: "-5px", cursor: 'pointer' }}
+                                /></a>
+                            <ReactTooltip place="bottom" type="dark" effect="float" />
                         </p>
                     </div>
                 </div>
@@ -24,12 +27,14 @@ export const ProjectDividendDetail = (props) => {
             <div className="col">
                 <div className="row">
                     <div className="col-auto pl-0">
-                        <p className="detail-2" style={{display: "inline"}}>
-                            Estimasi Dividen <i className="iconify" data-icon="ant-design:info-circle-outlined"
-                                                data-toggle="tooltip"
-                                                style={{fontSize: "24px", color: "#2dbe60", marginBottom: "-5px"}}
-                                                title="Disclaimer: kinerja masa lalu bukan merupakan indikasi kinerja masa depan. Estimasi dividen adalah ilustrasi berdasarkan proyeksi konservatif, dividen aktual akan tetap mengacu pada kinerja penerbit dan persetujuan Rapat Umum Pemegang Saham (RUPS). Estimasi dividen belum termasuk potensi capital gain dari kenaikan harga saham.">
-                                            </i>
+                        <p className="detail-2" style={{ display: "inline" }}>
+                            Estimasi Dividen&nbsp;
+                            <a
+                                data-tip="Disclaimer: kinerja masa lalu bukan merupakan indikasi kinerja masa depan. Estimasi dividen adalah ilustrasi berdasarkan proyeksi konservatif, dividen aktual akan tetap mengacu pada kinerja penerbit dan persetujuan Rapat Umum Pemegang Saham (RUPS). Estimasi dividen belum termasuk potensi capital gain dari kenaikan harga saham."
+                                data-event='click focus'>
+                                <i className="iconify" data-icon="ant-design:info-circle-outlined"
+                                    style={{ fontSize: "24px", color: "#2dbe60", marginBottom: "-5px", cursor: 'pointer' }} />
+                            </a>
                         </p>
                     </div>
                 </div>
