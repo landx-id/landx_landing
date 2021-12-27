7454601ae22b111d61c6c9bca5449285ca5f932a
ASUS\Fraza <adityafraja12@gmail.com>
2021-12-27 10:36:56 +0700

    change banner img in mobile device

--- landx-cuan/main.js
+++ landx-cuan/main.js
@@ -7,36 +7,37 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-(function () { var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; document.body.appendChild(script); })()
+// (function () { var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; document.body.appendChild(script); })()
 
 const bennerMobile = document.querySelector('#banner-mobile');
 let widthWindow = window.innerWidth
 window.addEventListener('resize', () => {
   widthWindow = window.innerWidth
   if (widthWindow >= 360 && widthWindow <= 576) {
-    bennerMobile.src = "./assets/images/Mobile-KV@2x.webp"
+    bennerMobile.src = "./assets/images/Mobile-KV-360.webp"
     bennerMobile.className = ("banner-img d-sm-none mt-3")
   }
   if (widthWindow < 360) {
-    bennerMobile.src = "./assets/images/Mobile-KV-320.webp"
+    bennerMobile.src = "./assets/images/Mobile-KV-360.webp"
     bennerMobile.className = ("banner-mobile d-sm-none mt-3")
   }
 })
 if (widthWindow >= 360 && widthWindow <= 576) {
-  bennerMobile.src = "./assets/images/Mobile-KV@2x.webp"
+  bennerMobile.src = "./assets/images/Mobile-KV-360.webp"
   bennerMobile.className = ("banner-img d-sm-none mt-3")
 }
 if (widthWindow < 360) {
-  bennerMobile.src = "./assets/images/Mobile-KV-320.webp"
+  bennerMobile.src = "./assets/images/Mobile-KV-360.webp"
   bennerMobile.className = ("banner-mobile d-sm-none mt-3")
 }
 
-    // const btnDownload = document.querySelector('#btn-download')
+// const btnDownloadPlaystore = document.querySelector('#btn-download-playstore')
+// const btnDownloadAppstore = document.querySelector('#btn-download-appstore')
 
-    // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    //   // true for mobile device
-    //   btnDownload.href = "https://landxapp.page.link/download"
-    // } else {
-    //   // false for not mobile device
-    //   btnDownload.href = "https://play.google.com/store/apps/details?id=store.numoney.landxapp"
-    // }
+// if (/iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+//   // true for mobile device
+//   btnDownloadAppstore.href = "https://apps.apple.com/id/app/landx-aplikasi-investasi/id1453823676?l=id"
+// } else {
+//   // false for not mobile device
+//   btnDownloadPlaystore.href = "https://play.google.com/store/apps/details?id=store.numoney.landxapp"
+// }
